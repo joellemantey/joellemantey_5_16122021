78a2538e6450575af20467bda5635fed3e3a6561
Joëlle MANETY <joelle.mantey@gmail.com>
2021-12-20 10:00:24 +0100

    liste des produits sur la page d'accueil

---
+++ front/js/index.js
@@ -0,0 +1,72 @@
+const baseURI = 'http://localhost:3000/api/products';
+
+/**
+ * Récupère tous les produits
+ * @returns Promise d'un tableau de produits
+ */
+const getAllProducts = () => {
+    return fetch(baseURI)
+        .then(function(res) { // controle de la réponse
+            if (res.ok) {
+                return res.json();
+            }
+        })
+        .then(function(value) { // traitement des données
+            // console.log(value);
+            return value;
+        })
+        .catch(function(err) {
+            console.error('Problème lors de la récupération des produits.', err);
+            // Une erreur est survenue
+        });
+}
+const items = document.getElementById("items");
+
+/**
+ * Fonction pour afficher un produit
+ * @param product
+ */
+const renderProduct = (product) => {
+    const productLink = document.createElement('a');
+    productLink.href = './product.html?id=' + product._id;
+    const container = document.createElement('article');
+    productLink.append(container);
+    const img = document.createElement('img');
+    img.src = product.imageUrl;
+    img.alt = product.altTxt;
+    container.append(img);
+    const title = document.createElement('h3');
+    title.className = 'productName';
+    title.textContent = product.name;
+    container.append(title);
+    const description = document.createElement('p');
+    description.className = 'productDescription';
+    description.textContent = product.description;
+    container.append(description)
+    items.append(productLink);
+}
+
+/**
+ * Fonction principal qui s'exécute au démarrage de la page.
+ */
+(async function() {
+    const products = await getAllProducts();
+    console.log('products', products);
+    for (const product of products){
+        renderProduct(product)
+    }
+
+})();
+
+// const main = async () => await getAllProducts();
+// main();
+
+// const str = "http://localhost:3000/images/kanap01.jpeg";
+// let url = new URL(str);
+// let search_params = new URLSearchParams(url.search);
+//
+// if (search_params.has('name')) {
+//     let name = search_params.get('name');
+//     console.log(name);
+// }
+
